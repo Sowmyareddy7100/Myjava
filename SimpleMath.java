2ab1ba835d46be8d89dc689895ebcf5748b6b6c8
Sowmyareddy7100 <your-email@example.com>
2025-04-03 23:34:28 -0400

    first commit

---
+++ SimpleMath.java
@@ -0,0 +1,8 @@
+public class SimpleMath {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int result = x * y;
+        System.out.println("The result is: " + result);
+    }
+}
