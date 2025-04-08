24870bc52dd5d4cba08c28a9d770edc49c535872
Sowmyareddy7100 <Sowmyareddy710@gmail.com>
2025-04-08 13:47:18 -0400

    Add simple Multiplication.java code

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+        // Define the two numbers you want to multiply
+        int number1 = 5;
+        int number2 = 10;
+
+        // Perform the multiplication
+        int product = number1 * number2;
+
+        // Print the result to the console
+        System.out.println("The product of " + number1 + " and " + number2 + " is: " + product);
+    }
+}
